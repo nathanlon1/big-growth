de569fd0a5eed7d5a17616bf9b535ff076db3541
Nathan Hanlon <nathan.hanlon@umconnect.umt.edu>
2021-09-15 14:38:27 -0600

    Added hobbies to NathanHanlon()

--- group.py
+++ group.py
@@ -10,6 +10,7 @@ def NathanHanlon():
     print("My name is Nathan Hanlon")
     print("I am a Sophomore at the University of Montana")
     print("I am majoring in Computer Science")
+    print("I enjoy biking, hiking, and snowboarding.")
 
 
 def MylesSteinmetz():
