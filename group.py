6e48255f6fd0dc23c17c348399fa53e3067c8c9e
Nathan Hanlon <nathan.hanlon@umconnect.umt.edu>
2021-09-15 14:34:40 -0600

    Added major to NathanHanlon()

--- group.py
+++ group.py
@@ -9,6 +9,7 @@ bigGrowth()
 def NathanHanlon():
     print("My name is Nathan Hanlon")
     print("I am a Sophomore at the University of Montana")
+    print("I am majoring in Computer Science")
 
 
 def MylesSteinmetz():
