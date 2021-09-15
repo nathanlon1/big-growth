a1571a67ffbde4e07a370d428cf19e1791b8beaa
Myles <ms164187@umconnect.umt.edu>
2021-09-15 14:44:37 -0600

    fourth commit

--- group.py
+++ group.py
@@ -12,4 +12,4 @@ def MylesSteinmetz():
     print("I am in my senior year")
     print("I am majoring in computer science")
     print("My hobbies include playing guitar and drums, hiking and backpacking")
-    
+    print("I grew up in Havre Montana, a small town just south of the Canadian border")
